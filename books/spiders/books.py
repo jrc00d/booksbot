8319a8bcfcfb51ec2a295b97d2df0b63d0944f71
jrc00d <jrc00d@gmail.com>
2018-01-14 07:34:31 -0600

    Update books.py

--- books/spiders/books.py
+++ books/spiders/books.py
@@ -1,30 +1,33 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from selenium import webdriver
 
+class gcdc(scrapy.Spider):
+    name = "gcdc_felony"
+    start_urls = ['http://publicaccess.co.galveston.tx.us/default.aspx']
 
-class BooksSpider(scrapy.Spider):
-    name = "books"
-    allowed_domains = ["books.toscrape.com"]
-    start_urls = [
-        'http://books.toscrape.com/',
-    ]
+    def __init__(self):
+        self.driver = webdriver.Firefox()
+        def parse(self, response):
+            self.driver.get(response.url)
+            while True:
+                criminal = self.driver.find_element_by_xpath('/html/body/table/tbody/tr[2]/td/table/tbody/tr[1]/td[2]/a[3]')
+                try:
+                    criminal.click()
+                    print ("Criminal Click Success")
+                    # get the data and write it to scrapy items
+                except:
+                    break
+                    print ("Criminal Click Fail")
 
     def parse(self, response):
-        for book_url in response.css("article.product_pod > h3 > a ::attr(href)").extract():
-            yield scrapy.Request(response.urljoin(book_url), callback=self.parse_book_page)
-        next_page = response.css("li.next > a ::attr(href)").extract_first()
-        if next_page:
-            yield scrapy.Request(response.urljoin(next_page), callback=self.parse)
+        for test in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').extract_first(),
+                'author': quote.css('small.author::text').extract_first(),
+                'tags': quote.css('div.tags a.tag::text').extract(),
+            }
 
-    def parse_book_page(self, response):
-        item = {}
-        product = response.css("div.product_main")
-        item["title"] = product.css("h1 ::text").extract_first()
-        item['category'] = response.xpath(
-            "//ul[@class='breadcrumb']/li[@class='active']/preceding-sibling::li[1]/a/text()"
-        ).extract_first()
-        item['description'] = response.xpath(
-            "//div[@id='product_description']/following-sibling::p/text()"
-        ).extract_first()
-        item['price'] = response.css('p.price_color ::text').extract_first()
-        yield item
+#self.driver.close()
+
+print ("spider ran and finished")
